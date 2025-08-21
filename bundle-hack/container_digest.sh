c7320bf73a6279f2221b13f068b0a071c805878d
Grzegorz Piotrowski <geopiotrowski@gmail.com>
2025-08-21 16:58:34 +0100

    Image pullspec for the operand and operator from the on-push pipeline

--- bundle-hack/container_digest.sh
+++ bundle-hack/container_digest.sh
@@ -1,5 +1,5 @@
 # Do not remove comment lines, they are there to reduce conflicts
 # Operator
-export OPERATOR_IMAGE_PULLSPEC='quay.io/redhat-user-workloads/aws-load-balancer-operator-tenant/aws-load-balancer-operator-grzpiotrowski:on-pr-a16ba056a7976fcac9ed51214c091b35483d3e76'
+export OPERATOR_IMAGE_PULLSPEC='quay.io/redhat-user-workloads/aws-load-balancer-operator-tenant/aws-load-balancer-operator-grzpiotrowski:65194b50b1da6adef45ea66bd849f4745655fb15'
 # Controller
-export OPERAND_IMAGE_PULLSPEC='quay.io/redhat-user-workloads/aws-load-balancer-operator-tenant/aws-load-balancer-controller-grzpiotrowski:on-pr-f6429bda8e7a30548d775123c2d583a02931b92f'
+export OPERAND_IMAGE_PULLSPEC='quay.io/redhat-user-workloads/aws-load-balancer-operator-tenant/aws-load-balancer-controller-grzpiotrowski:1c5c4c3aa6c5370fe00c4b9d2f5279aa8af1085d'
