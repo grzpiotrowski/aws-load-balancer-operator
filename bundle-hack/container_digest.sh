e40444c033e2b1b3b3569f408fc4b2e8368a8605
Grzegorz Piotrowski <geopiotrowski@gmail.com>
2025-09-03 15:25:55 +0100

    Add OLM Catalog for Konflux

--- bundle-hack/container_digest.sh
+++ bundle-hack/container_digest.sh
@@ -1,7 +1,7 @@
 # Do not remove comment lines, they are there to reduce conflicts
 # Operator
-export OPERATOR_IMAGE_PULLSPEC='quay.io/redhat-user-workloads/aws-load-balancer-operator-tenant/aws-lb-optr-1-2-rhel-9/aws-load-balancer-operator-container-aws-lb-optr-1-2-rhel-9@sha256:937c802224e8283f79186d3e849ebb2ed5a0d91d25346bd1b9a0e5fdc94ef806'
+export OPERATOR_IMAGE_PULLSPEC='quay.io/redhat-user-workloads/aws-load-balancer-operator-tenant/aws-lb-optr-1-3-rhel-9/aws-load-balancer-operator-container-aws-lb-optr-1-3-rhel-9@sha256:sha256:2329703f19cbbe2bfed2787d4b9d8a61c625321415fdda5d622a9473c1fe65b6'
 # Controller
-export OPERAND_IMAGE_PULLSPEC='quay.io/redhat-user-workloads/aws-load-balancer-operator-tenant/aws-lb-optr-1-2-rhel-9/aws-load-balancer-controller-container-aws-lb-optr-1-2-rhel-9@sha256:1de0313bc3bcfe9f4d7a531338f5a3df63bdd3f7f1b4371e1b7c309c16ff5a90'
+export OPERAND_IMAGE_PULLSPEC='quay.io/redhat-user-workloads/aws-load-balancer-operator-tenant/aws-lb-optr-1-3-rhel-9/aws-load-balancer-controller-container-aws-lb-optr-1-3-rhel-9@sha256:c0e8b265b2b3b73030ae33cfcdfd107552331005d39e76dd250a07526cddf489'
 # kube-rbac-proxy
 export KUBE_RBAC_PROXY_IMAGE_PULLSPEC='registry.redhat.io/openshift4/ose-kube-rbac-proxy@sha256:56653df97e4498ecd67b9645d75e44f65c625594e312dbcf0f09937b57d76010'
